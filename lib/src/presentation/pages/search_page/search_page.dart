24a6b879a27b666dbe30128dc7e6af746aac94b9
dolbobob <dolbobob00@gmail.com>
2025-02-27 18:52:26 +0600

    Added media controller, video playing when cathicn webw or smth in image card/

--- lib/src/presentation/pages/search_page/search_page.dart
+++ lib/src/presentation/pages/search_page/search_page.dart
@@ -28,182 +28,185 @@ class SearchPage extends StatelessWidget {
         ),
       ),
       drawer: const MyCustomDrawer(),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SearchField(),
-          ),
-          ExpansionTile(
-            title: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Rules of tag writing',
-                ),
-                Icon(
-                  Icons.info,
-                ),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SearchField(),
             ),
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(
-                    25,
+            ExpansionTile(
+              title: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Rules of tag writing',
                   ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RichText(
-                    text: TextSpan(
-                      style: Theme.of(context).textTheme.bodyMedium,
-                      children: [
-                        TextSpan(
-                          text: '- It\'s better to start with small letter\n',
-                        ),
-                        TextSpan(
-                          text: '- Use underscope "_" and not use space " "\n',
-                        ),
-                        TextSpan(
-                          text:
-                              '- Sometimes you must use name and surname of character\n',
-                        ),
-                        TextSpan(
-                          text:
-                              'Something like: "zoro_roronoa" or "dayneris_targaryen"\n',
-                        ),
-                        TextSpan(
-                          text:
-                              '- Sometimes you must specify name of which anime talking\n',
-                        ),
-                        TextSpan(
-                          text:
-                              'Something like: "rem_(re:zero)" and "ram_(re:zero)"\n',
-                        ),
-                      ],
+                  Icon(
+                    Icons.info,
+                  ),
+                ],
+              ),
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(
+                      25,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: RichText(
+                      text: TextSpan(
+                        style: Theme.of(context).textTheme.bodyMedium,
+                        children: [
+                          TextSpan(
+                            text: '- It\'s better to start with small letter\n',
+                          ),
+                          TextSpan(
+                            text:
+                                '- Use underscope "_" and not use space " "\n',
+                          ),
+                          TextSpan(
+                            text:
+                                '- Sometimes you must use name and surname of character\n',
+                          ),
+                          TextSpan(
+                            text:
+                                'Something like: "zoro_roronoa" or "dayneris_targaryen"\n',
+                          ),
+                          TextSpan(
+                            text:
+                                '- Sometimes you must specify name of which anime talking\n',
+                          ),
+                          TextSpan(
+                            text:
+                                'Something like: "rem_(re:zero)" and "ram_(re:zero)"\n',
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Welcome to the Horny Anime App!',
-                  style: TextStyle(fontSize: 24),
-                ),
-                SizedBox(height: 20),
-                BlocBuilder<AnimePicsCubit, AnimePicsState>(
-                  bloc: bloc,
-                  builder: (context, state) {
-                    if (state is AnimePictureState) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(),
-                          borderRadius: BorderRadius.circular(
-                            25,
-                          ),
-                        ),
-                        child: ImageCard(
-                          imageUrl: state.pictureUrl,
-                          date: state.uploadedAt,
-                          source: state.source,
-                        ),
-                      );
-                    } else if (state is AnimeMultiplePicturesState) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(),
-                          borderRadius: BorderRadius.circular(
-                            25,
-                          ),
-                        ),
-                        child: SizedBox(
-                          width: double.infinity,
-                          height: MediaQuery.of(context).size.height * 0.6 +
-                              MediaQuery.of(context).padding.bottom,
-                          child: ListView.builder(
-                            itemCount: state.pictureUrls.length,
-                            scrollDirection: Axis.vertical,
-                            itemBuilder: (context, index) {
-                              return ImageCard(
-                                imageUrl: state.pictureUrls[index],
-                                date: state.uploadedAt[index],
-                                source: state.source[index],
-                              );
-                            },
-                          ),
-                        ),
-                      );
-                    } else if (state is AnimePictureError) {
-                      return Column(
-                        children: [
-                          Text(
-                            'Problem was caught.',
-                            style: Theme.of(context)
-                                .textTheme
-                                .headlineMedium
-                                ?.copyWith(
-                                  color: Colors.red,
-                                ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Text(
-                              'Try to clear tag\'s, add tag\'s by one per time, sometimes base doesnt have multiple tag\'s like \n "Maid + Gif + Oppai"',
-                              maxLines: 3,
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                          ),
-                          ExpansionTile(
-                            leading: Icon(
-                              Icons.error,
-                            ),
-                            iconColor: Theme.of(context).colorScheme.error,
-                            title: Text(
-                              'See full error code',
-                            ),
-                            children: [
-                              Text(
-                                state.message,
-                              ),
-                            ],
-                          ),
-                        ],
-                      );
-                    } else if (state is AnimePicsLoading) {
-                      return const CustomLoadingCircle(
-                        size: 100.0,
-                      );
-                    }
-                    return Center(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(),
-                          borderRadius: BorderRadius.circular(
-                            25,
-                          ),
-                        ),
-                        child: Text(
-                          'Be carefull! This is a NSFW content!',
-                          style: TextStyle(
-                            fontSize: 24,
-                            color: Colors.red,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                )
               ],
             ),
-          ),
-        ],
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Welcome to the Horny Anime App!',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  SizedBox(height: 20),
+                  BlocBuilder<AnimePicsCubit, AnimePicsState>(
+                    bloc: bloc,
+                    builder: (context, state) {
+                      if (state is AnimePictureState) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(
+                              25,
+                            ),
+                          ),
+                          child: ImageCard(
+                            imageUrl: state.pictureUrl,
+                            date: state.uploadedAt,
+                            source: state.source,
+                          ),
+                        );
+                      } else if (state is AnimeMultiplePicturesState) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(
+                              25,
+                            ),
+                          ),
+                          child: SizedBox(
+                            width: double.infinity,
+                            height: MediaQuery.of(context).size.height * 0.6 +
+                                MediaQuery.of(context).padding.bottom,
+                            child: ListView.builder(
+                              itemCount: state.pictureUrls.length,
+                              scrollDirection: Axis.vertical,
+                              itemBuilder: (context, index) {
+                                return ImageCard(
+                                  imageUrl: state.pictureUrls[index],
+                                  date: state.uploadedAt[index],
+                                  source: state.source[index],
+                                );
+                              },
+                            ),
+                          ),
+                        );
+                      } else if (state is AnimePictureError) {
+                        return Column(
+                          children: [
+                            Text(
+                              'Problem was caught.',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headlineMedium
+                                  ?.copyWith(
+                                    color: Colors.red,
+                                  ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                'Try to clear tag\'s, add tag\'s by one per time, sometimes base doesnt have multiple tag\'s like \n "Maid + Gif + Oppai"',
+                                maxLines: 3,
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                            ),
+                            ExpansionTile(
+                              leading: Icon(
+                                Icons.error,
+                              ),
+                              iconColor: Theme.of(context).colorScheme.error,
+                              title: Text(
+                                'See full error code',
+                              ),
+                              children: [
+                                Text(
+                                  state.message,
+                                ),
+                              ],
+                            ),
+                          ],
+                        );
+                      } else if (state is AnimePicsLoading) {
+                        return const CustomLoadingCircle(
+                          size: 100.0,
+                        );
+                      }
+                      return Center(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(
+                              25,
+                            ),
+                          ),
+                          child: Text(
+                            'Be carefull! This is a NSFW content!',
+                            style: TextStyle(
+                              fontSize: 24,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/image_card/image_card.dart
+++ lib/widgets/image_card/image_card.dart
@@ -2,112 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:restful_solid_bloc/src/domain/url_service.dart';
 import 'package:restful_solid_bloc/widgets/image_card/share_button.dart';
-
-import '../cached_image.dart';
+import '../media_player/media_player.dart';
 import 'download_button.dart';
 
 class ImageCard extends StatelessWidget {
-  const ImageCard({
-    super.key,
-    this.source,
-    required this.imageUrl,
-    this.width,
-    this.height,
-    this.fit,
-    this.date,
-  });
   final String imageUrl;
-  final String? source;
   final String? date;
-  final double? width;
-  final double? height;
-  final BoxFit? fit;
+  final String? source;
+
+  const ImageCard({
+    Key? key,
+    required this.imageUrl,
+    this.date,
+    this.source,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CachedImage(
-          //! _nextFrame != null package:flutter/src/painting/image_stream.dart idk how to fix ths sht
-          imageUrl: imageUrl,
-          width: width ?? double.infinity,
-          height: height ?? MediaQuery.of(context).size.height * 0.45,
-          fit: fit ?? BoxFit.contain,
-        ),
-        Text(
-          date ?? '',
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: InkWell(
-            onTap: () => GetIt.I<IUrlService>().launchURL(
-              imageUrlOrSource: source ?? imageUrl,
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 16 / 9,
+            child: MediaPlayer(
+              url: imageUrl,
+              fit: BoxFit.cover,
             ),
-            child: Padding(
+          ),
+          if (date != null || source != null)
+            Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Row(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Icon(
-                    Icons.auto_awesome,
-                    size: 20,
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(2.5),
-                      child: LayoutBuilder(
-                        builder: (context, constraints) {
-                          return RichText(
-                            maxLines: 3,
-                            overflow: TextOverflow.ellipsis,
-                            text: TextSpan(
-                              children: [
-                                const TextSpan(
-                                  text: 'Source: ',
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                  ),
-                                ),
-                                TextSpan(
-                                  text: source ?? imageUrl,
-                                  style: const TextStyle(
-                                    color: Colors.blue,
-                                  ),
-                                ),
-                                const TextSpan(
-                                  text:
-                                      '\n (tap to open, sometimes page may not exist)',
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                    fontSize: 10,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        },
-                      ),
+                  if (date != null)
+                    Text(
+                      'Uploaded: $date',
+                      style: Theme.of(context).textTheme.bodySmall,
                     ),
-                  ),
+                  if (source != null)
+                    Text(
+                      'Source: $source',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
                 ],
               ),
             ),
-          ),
-        ),
-        Row(
-          children: [
-            IconButton(
-              icon: const Icon(Icons.favorite),
-              onPressed: () {},
-            ),
-            ShareButton(
-              url: imageUrl,
-              text: 'Ayo check this out! (from my app \'hornet\')',
-            ),
-            DownloadButton(
-              url: imageUrl,
-            ),
-          ],
-        )
-      ],
+        ],
+      ),
     );
   }
 }
